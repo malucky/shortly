17ce94dc4db8b89f672fbbc3fd70287050f938fa
Hack Reactor Students <rhm262@gmail.com>
2014-02-05 15:06:27 -0800

    Implement filter by keyword

--- public/client/linksView.js
+++ public/client/linksView.js
@@ -3,7 +3,8 @@ Shortly.LinksView = Backbone.View.extend({
   className: 'links',
 
   events: {
-    'change .filterLinks': 'sortBy'
+    'change .filterLinks input[name=sortBy]': 'sortBy',
+    'keyup .filterLinks input[name=searchLinks]': 'filterLinks'
   },
 
   initialize: function(){
@@ -18,13 +19,18 @@ Shortly.LinksView = Backbone.View.extend({
                       <form class="filterLinks">\
                       <input type="radio" name="sortBy" value="visits" checked>Visits</input>\
                       <input type="radio" name="sortBy" value="created_at">Time</input>\
+                      <input type="text" name="searchLinks" placeholder="search links"></input>\
                       </form>\
                       <div class="linksDisplay"></div>');
     return this;
   },
 
-  addAll: function(){
-    this.collection.forEach(this.addOne, this);
+  addAll: function(filteredLinks){
+    if(filteredLinks){
+      filteredLinks.forEach(this.addOne, this);
+    } else {
+      this.collection.forEach(this.addOne, this);
+    }
   },
 
   addOne: function(item){
@@ -39,6 +45,15 @@ Shortly.LinksView = Backbone.View.extend({
 
     this.$el.find('.linksDisplay').html('');
     this.addAll();
+  },
+
+  filterLinks: function(){
+    var searchTerm = $('input[name=searchLinks]', '.filterLinks').val();
+    var filteredLinks = this.collection.filter(function(link){
+      return link.get('title').toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+    });
+    this.$el.find('.linksDisplay').html('');
+    this.addAll(filteredLinks);
   }
 
 });
